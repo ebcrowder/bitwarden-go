81ee2854ee83f88ef317a391335e387baa2ff618
ebcrowder <eric@ebcrowder.dev>
2021-03-01 20:29:07 -0800

    Add db dir

---
+++ internal/db/mock/mock.go
@@ -0,0 +1,74 @@
+package mock
+
+import (
+	bw "github.com/ebcrowder/bitwarden-go/internal/common"
+	_ "github.com/lib/pq"
+)
+
+// mock database used for testing
+type MockDB struct {
+	Username        string
+	Password        string
+	RefreshToken    string
+	TwoFactorSecret string
+	KdfIterations   int
+}
+
+func (db *MockDB) Init() error {
+	return nil
+}
+
+func (db *MockDB) SetDir(d string) {
+}
+
+func (db *MockDB) Open() error {
+	return nil
+}
+
+func (db *MockDB) Close() {
+}
+
+func (db *MockDB) UpdateAccountInfo(acc bw.Account) error {
+	return nil
+}
+
+func (db *MockDB) GetCipher(owner string, ciphID string) (bw.Cipher, error) {
+	return bw.Cipher{}, nil
+}
+
+func (db *MockDB) GetCiphers(owner string) ([]bw.Cipher, error) {
+	return nil, nil
+}
+
+func (db *MockDB) NewCipher(ciph bw.Cipher, owner string) (bw.Cipher, error) {
+	return bw.Cipher{}, nil
+
+}
+
+func (db *MockDB) UpdateCipher(newData bw.Cipher, owner string, ciphID string) error {
+	return nil
+}
+
+func (db *MockDB) DeleteCipher(owner string, ciphID string) error {
+	return nil
+}
+
+func (db *MockDB) AddAccount(acc bw.Account) error {
+	return nil
+}
+
+func (db *MockDB) GetAccount(username string, refreshtoken string) (bw.Account, error) {
+	return bw.Account{Email: db.Username, MasterPasswordHash: db.Password, RefreshToken: db.RefreshToken, TwoFactorSecret: db.TwoFactorSecret, KdfIterations: db.KdfIterations}, nil
+}
+
+func (db *MockDB) AddFolder(name string, owner string) (bw.Folder, error) {
+	return bw.Folder{}, nil
+}
+
+func (db *MockDB) GetFolders(owner string) ([]bw.Folder, error) {
+	return nil, nil
+}
+
+func (db *MockDB) Update2FAsecret(secret string, email string) error {
+	return nil
+}
